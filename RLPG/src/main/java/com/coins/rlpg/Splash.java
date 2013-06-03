c5b2797d0ed9523d58d05885d62a07379ff60fd4
Adam <adamjones077@gmail.com>
2013-06-03 19:11:46 -0400

    Initial Commit

---
+++ RLPG/src/main/java/com/coins/rlpg/Splash.java
@@ -0,0 +1,13 @@
+package com.coins.rlpg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Adam on 6/3/13.
+ */
+public class Splash extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
